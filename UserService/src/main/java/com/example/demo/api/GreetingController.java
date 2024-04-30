c2edf487b3300f6c23f530fd4158e450289ed0f3
doldo <doldo@BOOK-8IMR033M78>
2024-04-30 14:38:23 +0900

    user-service

---
+++ UserService/src/main/java/com/example/demo/api/GreetingController.java
@@ -0,0 +1,47 @@
+package com.example.demo.api;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Tag(name = "사용자 서비스 V1", description = "사용자 관련 API(기본)")
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/user-service/v1")
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@Operation(summary = "사용자 조회", 
+			   description = "사용자를 조회한 정보를 반환함", 
+			   responses = {
+			                 @ApiResponse(responseCode = "200", description = "조회된 사용자 정보를 반환함.") 
+			                 })
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		log.info("Hello sleuth");
+		log.info("Bye sleuth");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@Operation(summary = "사용자 등록", 
+			   description = "사용자 정보를 등록합니다.", 
+			   responses = {
+			                 @ApiResponse(responseCode = "200", description = "등록결과를 반환함.") 
+			                 })
+	@PostMapping("/register")
+	public void register() {
+		log.info("user-service register");
+	}
+}
---
+++ UserService/src/main/java/egov/common/swagger/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package egov.common.swagger;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+
+@OpenAPIDefinition(
+        info = @Info(title = "User-Service API 명세서",
+                description = "파일럿을 위한 사용자 샘플 서비스 API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+	 @Bean
+	 public GroupedOpenApi getItemApi() { 
+		// "/user-service/**" 경로에 매칭되는 API를 'user'으로 그룹화하여 문서화한다. http://localhost/user-service/api-docs/user 으로 API 목록을 가져올수 있다
+	        return GroupedOpenApi
+	            .builder()
+	            .group("user")
+	            .pathsToMatch("/user-service/**")
+	            .build();
+
+	}
+	 
+}
