96f364dbe2abb3aa50f994c2ba100c9dfd044c68
doldo <doldo@BOOK-8IMR033M78>
2024-05-01 16:19:08 +0900

    1

--- PaymentService/src/main/java/com/example/demo/PaymentServiceController.java
+++ PaymentService/src/main/java/com/example/demo/PaymentServiceController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +23,11 @@ public class PaymentServiceController {
 	 	@Value("${server.port}")
 	    private String port;
 
+	 	@Operation(summary = "결재상태 확인", 
+				   description = "결재상태를 반환하는 인터페이스", 
+				   responses = {
+				                 @ApiResponse(responseCode = "200", description = "결재상태를 반환함.") 
+				                 })
 	    @GetMapping("/check")
 	    public String check() {
 	    	 log.trace("TRACE 로그!!");
