c1fe4faf7a262e198f56a5eded3d3cb42d2c3fa1
doldo <doldo@BOOK-8IMR033M78>
2024-05-01 16:07:58 +0900

    payment

---
+++ PaymentService/src/main/java/com/example/demo/PaymentServiceController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Tag(name = "결재 서비스 V1", description = "결재 관련 API(기본)")
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/v1/payment-service")
+@RestController
+public class PaymentServiceController {
+		
+	 	@Value("${server.port}")
+	    private String port;
+
+	    @GetMapping("/check")
+	    public String check() {
+	    	 log.trace("TRACE 로그!!");
+	    	 log.debug("DEBUG 로그!!");
+	    	 log.info("INFO 로그!!");
+	    	 log.warn("WARN 로그!!");
+	    	 log.error("ERROR 로그!!");
+	        return String.format("First Service on PORT %s", port);
+	       
+	    }
+}
---
+++ PaymentService/src/main/java/com/example/demo/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Payment-Service API 명세서",
+                description = "파일럿을 위한 결제 샘플 서비스 API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+	 @Bean
+	 public GroupedOpenApi getItemApi() { 
+		// "/payment-service/**" 경로에 매칭되는 API를 'user'으로 그룹화하여 문서화한다. http://localhost/payment-service/api-docs/user 으로 API 목록을 가져올수 있다
+	        return GroupedOpenApi
+	            .builder()
+	            .group("결제서비스")
+	            .pathsToMatch("/payment-service/**")
+	            .build();
+
+	}
+	 
+}
