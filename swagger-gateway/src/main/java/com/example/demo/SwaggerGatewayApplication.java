982029d8a251f58cfc93f46586661cd9a12b3741
doldo <doldo@BOOK-8IMR033M78>
2024-05-02 07:35:43 +0900

    1

--- swagger-gateway/src/main/java/com/example/demo/SwaggerGatewayApplication.java
+++ swagger-gateway/src/main/java/com/example/demo/SwaggerGatewayApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @EnableDiscoveryClient 
 @SpringBootApplication
@@ -12,4 +14,8 @@ public class SwaggerGatewayApplication {
 		SpringApplication.run(SwaggerGatewayApplication.class, args);
 	}
 
+	@Bean
+	ForwardedHeaderFilter forwardedHeaderFilter() {
+	   return new ForwardedHeaderFilter();
+	} 
 }
