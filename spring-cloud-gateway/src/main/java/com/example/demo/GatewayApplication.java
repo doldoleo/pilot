ff545980415701760d6f12a0510a134dc41981fe
doldo <doldo@BOOK-8IMR033M78>
2024-05-02 22:11:36 +0900

    scg

--- spring-cloud-gateway/src/main/java/com/example/demo/GatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/example/demo/GatewayApplication.java
@@ -2,16 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import reactor.core.publisher.Hooks;
 
-@EnableDiscoveryClient 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		 Hooks.enableAutomaticContextPropagation();
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
