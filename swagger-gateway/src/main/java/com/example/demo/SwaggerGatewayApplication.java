14161de28d1460f05ad9503566e86b3ccd26cf2c
doldo <doldo@BOOK-8IMR033M78>
2024-05-02 20:04:58 +0900

    eureka 삭제

--- swagger-gateway/src/main/java/com/example/demo/SwaggerGatewayApplication.java
+++ swagger-gateway/src/main/java/com/example/demo/SwaggerGatewayApplication.java
@@ -2,11 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
-@EnableDiscoveryClient 
 @SpringBootApplication
 public class SwaggerGatewayApplication {
 
