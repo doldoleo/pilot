5ad7948cf1f1967f461057dde791a51f48596d2c
doldo <doldo@BOOK-8IMR033M78>
2024-06-12 10:32:22 +0900

    1

--- scg-ui/src/main/java/fivefinger/web/ClientController.java
+++ scg-ui/src/main/java/fivefinger/web/ClientController.java
@@ -37,21 +37,13 @@ public class ClientController {
 	
 	@GetMapping("/payment")
 	public String payment(Model model, HttpSession session) {
-		
-		// 헤더 설정
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-	    
-	    OAuthToken token = (OAuthToken)session.getAttribute("userSession");
-	    
-	    headers.add("Authorization","Bearer "+ token.getAccessToken());
-
+		HttpHeaders headers = getHeaders(session);
 	    HttpEntity<Object> request = new HttpEntity<Object>(headers);
-
-		
 		URI uri = UriComponentsBuilder
 				.fromUriString(api_gateway_url)
-				.path("/api/v1/payment/check").encode().build()
+				.path("/api/v1/payment/check")
+				.encode()
+				.build()
 				.toUri();
 
 		ResponseEntity<String> responseEntity = restTemplate.exchange(uri,  HttpMethod.GET, request, String.class);
@@ -62,12 +54,40 @@ public class ClientController {
 		return "pages/payment";
 }
 
-	@GetMapping("/user")
-	public String greeting(Model model) {
-		URI uri = UriComponentsBuilder.fromUriString(api_gateway_url).path("/api/v1/user/greeting")
-				.queryParam("name", "Gil-Dong").encode().build().toUri();
+	@GetMapping("/user/greeting")
+	public String greeting(Model model, HttpSession session) {
+		HttpHeaders headers = getHeaders(session);
+		HttpEntity<Greeting> request = new HttpEntity<Greeting>(headers);
+		
+		URI uri = UriComponentsBuilder
+					.fromUriString(api_gateway_url)
+					.path("/api/v1/user/greeting")
+				    .queryParam("name", "Gil-Dong")
+				    .encode()
+				    .build()
+				    .toUri();
 
-		ResponseEntity<Greeting> responseEntity = restTemplate.getForEntity(uri, Greeting.class);
+		ResponseEntity<Greeting> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, request, Greeting.class);
+		Greeting greeting = responseEntity.getBody();
+		model.addAttribute("greeting", greeting);
+
+		return "pages/user";
+	}
+	
+	@GetMapping("/user/nogreeting")
+	public String nogreeting(Model model, HttpSession session) {
+		HttpHeaders headers = getHeaders(session);
+		HttpEntity<Greeting> request = new HttpEntity<Greeting>(headers);
+
+		URI uri = UriComponentsBuilder
+					.fromUriString(api_gateway_url)
+					.path("/api/v1/user/nogreeting")
+					.queryParam("name", "Gil-Dong")
+					.encode()
+					.build()
+					.toUri();
+
+		ResponseEntity<Greeting> responseEntity = restTemplate.exchange(uri,  HttpMethod.GET, request, Greeting.class);
 		Greeting greeting = responseEntity.getBody();
 		model.addAttribute("greeting", greeting);
 
@@ -75,16 +95,35 @@ public class ClientController {
 	}
 
 	@GetMapping("/merge")
-	public String merge(Model model) {
-		URI uri = UriComponentsBuilder.fromUriString(api_gateway_url).path("/api/v1/merge/call").encode().build()
+	public String merge(Model model, HttpSession session) {
+		HttpHeaders headers = getHeaders(session);
+		HttpEntity<Greeting> request = new HttpEntity<Greeting>(headers);
+		
+		URI uri = UriComponentsBuilder
+				.fromUriString(api_gateway_url)
+				.path("/api/v1/merge/call")
+				.encode()
+				.build()
 				.toUri();
 
-		ResponseEntity<Greeting> responseEntity = restTemplate.getForEntity(uri, Greeting.class);
+		ResponseEntity<Greeting> responseEntity = restTemplate.exchange(uri,  HttpMethod.GET, request, Greeting.class);
 		Greeting greeting = responseEntity.getBody();
 		model.addAttribute("greeting", greeting);
 
 		return "pages/merge";
 	}
 	
-	
+	/**
+	 * 토큰가져오기
+	 * @param session
+	 * @return
+	 */
+	private HttpHeaders getHeaders(HttpSession session) {
+		// 헤더 설정
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+	    OAuthToken token = (OAuthToken)session.getAttribute("userSession");
+	    headers.add("Authorization","Bearer "+ token.getAccessToken());
+	    return headers;
+	}
 }
