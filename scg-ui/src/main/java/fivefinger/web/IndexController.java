f6b40efaf556d7dcb5ff39638c8e1617b78c0115
doldo <doldo@BOOK-8IMR033M78>
2024-06-13 16:20:43 +0900

    1

--- scg-ui/src/main/java/fivefinger/web/IndexController.java
+++ scg-ui/src/main/java/fivefinger/web/IndexController.java
@@ -3,29 +3,11 @@ package fivefinger.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import fivefinger.oauth2.core.token.OAuthToken;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
 @Controller
 public class IndexController {
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
-	
-//	@GetMapping("/main")
-//	public String main(HttpServletRequest request, HttpServletResponse response) {
-//		HttpSession session = request.getSession(false);
-//		if (session != null ) {
-//			OAuthToken token = (OAuthToken)session.getAttribute("userSession");
-//			if (token != null ) {
-//				return "pages/main";
-//			}
-//		}
-//		return "/";
-//		
-//	}
 	
 }
